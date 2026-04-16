c404cb9809948bfb12a10b55cdb38f5039b4e302
Edwin Török <edwin@tarides.com>
2026-04-16 19:13:18 +0100

    Ethernet.{input/write}: track memory usage
    Signed-off-by: Edwin Török <edwin@tarides.com>

--- src/ethernet.ml
+++ src/ethernet.ml
@@ -57,6 +57,8 @@ open Lwt.Infix
 let src = Logs.Src.create "ethernet" ~doc:"Mirage Ethernet"
 module Log = (val Logs.src_log src : Logs.LOG)
 
+module PacketQueue = Mirage_net.PacketQueue
+
 module Make (Netif : Mirage_net.S) = struct
 
   type error = [ `Exceeds_mtu | `Netif of Netif.error ]
@@ -64,8 +66,13 @@ module Make (Netif : Mirage_net.S) = struct
     | `Exceeds_mtu -> Fmt.string ppf "exceeds MTU"
     | `Netif e -> Netif.pp_error ppf e
 
+
   type t = {
     netif: Netif.t;
+    device: PacketQueue.(device t);
+    input:  PacketQueue.(input t);
+    output: PacketQueue.(output t);
+    promise: PacketQueue.(promise t)
   }
 
   let mac t = Netif.mac t.netif
@@ -73,12 +80,16 @@ module Make (Netif : Mirage_net.S) = struct
 
   let input ~arpv4 ~ipv4 ~ipv6 t frame =
     let open Ethernet_packet in
+    let open Mirage_net in
     let of_interest dest =
       Macaddr.compare dest (mac t) = 0 || not (Macaddr.is_unicast dest)
     in
+    PacketQueue.on_input t.input frame;
+    let res =
     match Unmarshal.of_cstruct frame with
     | Ok (header, payload) when of_interest header.destination ->
       begin
+        PacketQueue.on_promise t.promise ~size_in_bytes:(Cstruct.length frame) @@
         match header.Ethernet_packet.ethertype with
         | `ARP -> arpv4 payload
         | `IPv4 -> ipv4 payload
@@ -88,8 +99,17 @@ module Make (Netif : Mirage_net.S) = struct
     | Error s ->
       Log.debug (fun f -> f "dropping Ethernet frame: %s" s);
       Lwt.return_unit
+    in
+    (* check before allocating next packet, while we still have a chance to avoid [OutOfMemory] *)
+    if PacketQueue.get_free_bytes t.input <= 0 then begin
+      Gc.compact ();
+      Log.debug (fun f -> f "Memory pressure on input. After compaction: %d"
+        (PacketQueue.get_free_bytes t.input))
+    end;
+    res
 
   let write t ?src destination ethertype ?size payload =
+    let open Mirage_net in
     let source = match src with None -> mac t | Some x -> x
     and eth_hdr_size = Ethernet_packet.sizeof_ethernet
     and mtu = mtu t
@@ -104,6 +124,7 @@ module Make (Netif : Mirage_net.S) = struct
       let size = eth_hdr_size + size in
       let hdr = { Ethernet_packet.source ; destination ; ethertype } in
       let fill frame =
+        PacketQueue.on_output t.output frame;
         match Ethernet_packet.Marshal.into_cstruct hdr frame with
         | Error msg ->
           Log.err (fun m -> m "error %s while marshalling ethernet header into allocated buffer" msg);
@@ -112,6 +133,14 @@ module Make (Netif : Mirage_net.S) = struct
           let len = payload (Cstruct.shift frame eth_hdr_size) in
           eth_hdr_size + len
       in
+      (* check before allocating, while we still have a chance to avoid [OutOfMemory] *)
+      if PacketQueue.get_free_bytes t.promise <= 0 ||
+         PacketQueue.get_free_bytes t.output <= 0
+      then begin
+        Gc.compact ();
+        Log.debug (fun f -> f "Memory pressure on output. After compaction: %d" (PacketQueue.get_free_bytes t.promise));
+      end;
+      PacketQueue.on_promise t.promise ~size_in_bytes:size @@
       Netif.write t.netif ~size fill >|= function
       | Ok () -> Ok ()
       | Error e ->
@@ -119,7 +148,12 @@ module Make (Netif : Mirage_net.S) = struct
         Error (`Netif e)
 
   let connect netif =
-    let t = { netif } in
+    let parent = PacketQueue.make_device () in
+    let size_in_bytes = Netif.mtu netif + Ethernet_packet.sizeof_ethernet in
+    let input = PacketQueue.make_input ~parent ~size_in_bytes ()
+    and output = PacketQueue.make_output ~parent ()
+    and promise = PacketQueue.make_promise ~parent () in
+    let t = { netif; device = parent; input; output; promise } in
     Log.info (fun f -> f "Connected Ethernet interface %a" Macaddr.pp (mac t));
     Lwt.return t
 
